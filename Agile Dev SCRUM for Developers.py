b07f306b3807d675da1178b49f46a96aaab6eb4a
vaejiang19960307@gmail.com <vaejiang19960307@gmail.com>
2023-08-01 12:48:24 +0000

    add

---
+++ Agile Dev SCRUM for Developers.py
@@ -0,0 +1,105 @@
+# Databricks notebook source
+# MAGIC %md 
+# MAGIC
+# MAGIC #### Agility
+# MAGIC - Agility in software development is a mindset
+# MAGIC
+# MAGIC #### Product Owner
+# MAGIC - needs to understand tech enought to communicate effectively
+# MAGIC - senior BA are good candidates
+# MAGIC - Product Owner vs Project Manager
+# MAGIC   - Project Manager is not part of SCRUM team
+# MAGIC   - Organization with SCRUM has project manager outside of SCRUM team
+# MAGIC
+# MAGIC #### All team member in SCRUM team share management responsibility
+# MAGIC
+# MAGIC #### SCRUM Team Member
+# MAGIC - In SCRUM, specialist is welcome to be part of the team
+# MAGIC - Specialist can be `I shape` (one deep understanding of a tech) or `T shape` (one deep understanding of one area and fair understanding of others)
+# MAGIC - *Optimal team size follows two pizza rule. number of team member can fed by two pizza*
+# MAGIC - Two Types of Teams
+# MAGIC   - Feature Team
+# MAGIC     - this type of team can handle diverse range of project
+# MAGIC   - Component Team
+# MAGIC     - this type of team is specialized on one type of task such as data analytics
+# MAGIC
+# MAGIC #### SCRUM Master
+# MAGIC - Responsibility
+# MAGIC   - De-escalate team argument
+# MAGIC   - 
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC
+# MAGIC #### Product Backlog
+# MAGIC - Product backlog is prioritized by product owner
+# MAGIC - **a useful way of prioritize the backlog is take a look at each items in backlog regarding how fine-grain each item is**
+# MAGIC - product backlog exists as long as the product exist
+# MAGIC
+# MAGIC #### Sprint Backlog
+# MAGIC - SCRUM works in sprint fashion, so each sprint there is a `sprint backlog`
+# MAGIC
+# MAGIC #### Product Implement
+# MAGIC - Integrated functional software that meets the `definition of done`, and it is ready to be released
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC
+# MAGIC #### Component in a Sprint
+# MAGIC - Backlog Refinement
+# MAGIC   - Loosely defined in SCRUM guild
+# MAGIC   - PO and Dev Team need attend
+# MAGIC   - can be conducted by an event and should take less than `10%` of the dev team's time
+# MAGIC   - Purposes
+# MAGIC     - adding items to backlog 
+# MAGIC     - review priority 
+# MAGIC     - clarify requirement
+# MAGIC - Sprint Planning
+# MAGIC   - Usually takes 2 to 8 hours
+# MAGIC   - two parts of the sprint planning are `define sprint goal` and `how to achieve goal in the sprint`
+# MAGIC   - **Sprint Goal has to be SMART goal**
+# MAGIC   - It is team member's responsbility to add detail plan / task; 
+# MAGIC   - **the ultimate goal of sprint planning is to set sprint backlog**
+# MAGIC - Daily Standup
+# MAGIC   - SCRUM master can facilitate / participate but not mando; dev team can conduct it anyway they want
+# MAGIC   - **duration should be 15 mins, it SHOULD NOT become problem solving session, it SHOULD NOT involve any stakeholder just dev team**
+# MAGIC   - answers 
+# MAGIC     - what I did previous day
+# MAGIC     - what I plan to do today
+# MAGIC     - Are there any roadblock
+# MAGIC - Sprint Review
+# MAGIC   - It is an event that SCRUM team and external stakeholders - `4 hours for 1 month sprint (shorter for shorter sprint length)`
+# MAGIC   - Answer `where we stand - progress?` and `what next?`
+# MAGIC   - Purposes: 
+# MAGIC     - Inspect product increment (items finished in sprint's backlog)
+# MAGIC - Sprint Retrospective
+# MAGIC   - **inspect anything other than product increment - AKA team performance review**
+# MAGIC   - `what went well`, `what not went well`, and `what can be better`
+# MAGIC   - Don't skip this component
+# MAGIC
+# MAGIC #### Objective of a Sprint
+# MAGIC - Each sprint must end with `product increment` which might released to stakeholders
+# MAGIC   - `product increment` should be end to end functional and provide business value
+# MAGIC
+# MAGIC #### Special types of Sprint
+# MAGIC - `Sprint Zero`: the very first sprint that dedicate to sprint planning
+# MAGIC - `Hardening Sprint`: the sprint dedicate to fix bugs and fine tunning features
+# MAGIC
+# MAGIC ### Technicle Debt
+# MAGIC - What is tech debt? 
+# MAGIC   - when you create a feature for the first time, you didn't realize how often this feature will be reused; so you need rebuild and refactor your code for better scalability
+# MAGIC   - when important programming language has a major version update and you need adapt your code to newer version
+# MAGIC   - You need tranform a quick short term solution to long term friendly code
+# MAGIC - How to address tech debt 
+# MAGIC   - make those debts visible to stakeholders, they need understand why it is important to address those debt
+# MAGIC   - address ASAP
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC
+# MAGIC #### Certification 
+# MAGIC - [Professsional SCRUM Master](https://www.scrum.org/professional-scrum-certifications)
+# MAGIC - [certified SCRUM master](https://www.scrumalliance.org/get-certified)
